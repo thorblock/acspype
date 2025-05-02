3a74cfcb1ecee54b3e6997aa5c9edd1bf11c7098
Ian Black <blackia@oregonstate.edu>
2025-05-02 10:39:27 -0700

    Add poc

--- acspype/experimental.py
+++ acspype/experimental.py
@@ -16,10 +16,10 @@ def compute_alh_676(a_p_650: xr.DataArray, a_p_676: xr.DataArray, a_p_715: xr.Da
     return alh
 
 
-def compute_chl_alh(a_p_650: xr.DataArray,
-                    a_p_676: xr.DataArray,
-                    a_p_715: xr.DataArray,
-                    alh_star: float = 0.0104) -> xr.DataArray:
+def estimate_chl(a_p_650: xr.DataArray,
+                 a_p_676: xr.DataArray,
+                 a_p_715: xr.DataArray,
+                 alh_star: float = 0.0104) -> xr.DataArray:
     """
     Compute chlorophyll-a from absorption line height via Roesler and Barnard 2013.
     https://www.sciencedirect.com/science/article/pii/S2211122013000509
@@ -36,3 +36,36 @@ def compute_chl_alh(a_p_650: xr.DataArray,
     alh = a_p_676 - abl  #EQ 2 in Roesler and Barnard 2013
     chl_alh = alh / alh_star #EQ 3 in Roesler and Barnard 2013
     return chl_alh
+
+
+def estimate_poc(c_p_660: xr.DataArray, slope_offset: str | tuple | list) -> xr.DataArray:
+    """
+    Compute particulate organic carbon from particle beam attenuation at 660nm using a provided slope and offset.
+    If the slope_offset value is a tuple or list, the first value is the slope and the second value is the offset.
+
+    If the slope_offset value is a string, a predefined slope and offset will be used. Strings represent the first
+    author and the year of the publication.
+
+    :param c_p_660: c_p at 660 nm.
+    :param slope_offset: A string indicating the literary source for the slope and offset or
+        a tuple or list containing the slope and offset values.
+    :return: Estimated particulate organic carbon in mg/m3.
+    """
+
+    if isinstance(slope_offset, str):
+        if slope_offset == 'gardner2006': # Gardner et al. 2006, Global, All Seasons
+            slope = 381
+            offset = 9.4
+        elif slope_offset == 'stramski2008': # Stramski et al. 2008, Fall, Pacific, Atlantic
+            slope = 458
+            offset = 10.7
+        elif slope_offset == 'behrenfeld2006': # Behrenfeld and Boss 2006, Fall, Equatorial Pacific
+            slope = 585
+            offset = 7.6
+        elif slope_offset == 'goni2021': # Goni et al, 2021, August, Oregon Shelf/Slope
+            slope = 39.32 * 12.01 # 39.32 is the mean of values from W1108C, Table 11, Goni et al. 2021
+            offset = 0
+    elif isinstance(slope_offset, tuple | list):
+        slope, offset = slope_offset
+    poc = slope * c_p_660 + offset
+    return poc
