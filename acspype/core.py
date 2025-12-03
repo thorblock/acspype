322c88a54f1eb4a78f00dd5521c45b3eb3a6cee9
Ian Black <blackia@oregonstate.edu>
2025-12-03 11:59:54 -0800

    Update core.py.
    - Updates doc strings.
    - Changes NC_ENCODING to be a value that takes up less memory.

--- acspype/core.py
+++ acspype/core.py
@@ -2,7 +2,7 @@
 
 NUM_PAT = r"[+-]?[0-9]*[.]?[0-9]+"  # REGEX for any number, float or int, positive or negative.
 
-# ---------- Packet Handling ---------- #
+# ---------- PACKET HANDLING ---------- #
 PACKET_REGISTRATION = b'\xff\x00\xff\x00'  # Start of every ACS packet.
 PAD_BYTE = b'\x00'  # End of every ACS packet.
 WVL_BYTE_OFFSET = 4 + 2 + 1 + 1 + 1 + 3 + 2 + 2 + 2 + 2 + 2 + 2 + 2 + 4 + 1  # See Process Data section in ACS manual.
@@ -11,8 +11,8 @@ PACKET_HEAD = '!4cHBBl7HIBB'  # struct descriptor for the first 31 bytes of an A
 PACKET_TAIL = 'Hx'  # struct descriptor for the last 3 bytes of an ACS packet (checksum + pad byte).
 LPR = len(PACKET_REGISTRATION)
 
-# ---------- File Creation ---------- #
-NC_ENCODING = {'time': {'units': 'nanoseconds since 1900-01-01'}}  # Recommended xr.Dataset to netcdf encoding for time.
+# ---------- FILE CREATION ---------- #
+NC_ENCODING = {'time': {'units': 'milliseconds since 1970-01-01'}}  # Recommended xr.Dataset to netcdf encoding for time.
 
 # ---------- PHYSICAL QUANTITIES ---------- #
 EST_FLOW_CELL_VOLUME = 30  # in mL, from the ACS Protocol Document, Rev Q.
